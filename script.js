1a2b62ff90d91ce6de837bd023c8dd39daca4584
Foltrex <fpm.chernenkAV@bsu.by>
2024-07-04 23:13:51 +0300

    Add componay experience modal

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ navbarLinks.forEach(link => link.addEventListener('click', () => {
     const sideMenuButton = document.getElementById('side-menu');
     sideMenuButton.click();
 }))
+
+const showCompanyExperienceButtons = document.querySelectorAll('section#experience .job-info > button');
+showCompanyExperienceButtons.forEach(btn => {
+    const companyExperienceModal = btn.nextElementSibling;
+    const closeModalButton = companyExperienceModal?.children[0];
+    btn.addEventListener('click', () => companyExperienceModal.showModal());
+    closeModalButton.addEventListener('click', () => companyExperienceModal.close());
+})
